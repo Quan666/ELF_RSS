a9c2c6f00835cbd9b9d42854d712c7afc1c0a041
Quan666 <Quan666@users.noreply.github.com>
2021-02-24 22:13:36 +0800

    fix bugs

--- src/plugins/ELF_RSS2/start.py
+++ src/plugins/ELF_RSS2/start.py
@@ -2,6 +2,7 @@ import nonebot
 from nonebot import logger, on_metaevent
 from nonebot.adapters.cqhttp import Bot, Event
 
+from bot import driver
 from .config import config
 from .RSSHUB import RSS_class
 from .RSSHUB import del_cache as DC
@@ -24,26 +25,28 @@ async def start():
         for rss_tmp in rss_list:
             await RT.addJob(rss_tmp)  # 创建检查更新任务
         await bot.send_msg(message_type='private', user_id=str(list(config.superusers)[0]),
-                           message='ELF_RSS 订阅器启动成功！\nVersion: {}\nAuthor：Quan666\nhttps://github.com/Quan666/ELF_RSS'.format(config.version))
+                           message='ELF_RSS 订阅器启动成功！\nVersion: {}\nAuthor：Quan666\nhttps://github.com/Quan666/ELF_RSS'.format(
+                               config.version))
         logger.info('ELF_RSS 订阅器启动成功！')
     except Exception as e:
         await bot.send_msg(message_type='private', user_id=str(list(config.superusers)[0]),
-                           message='第一次启动，你还没有订阅，记得添加哟！\nVersion: {}\nAuthor：Quan666\nhttps://github.com/Quan666/ELF_RSS'.format(config.version))
+                           message='第一次启动，你还没有订阅，记得添加哟！\nVersion: {}\nAuthor：Quan666\nhttps://github.com/Quan666/ELF_RSS'.format(
+                               config.version))
         logger.info('第一次启动，你还没有订阅，记得添加哟！')
         # logger.debug(e)
 
 
-# region 启动问好
 async def check_first_connect(bot: Bot, event: Event, state: dict) -> bool:
-    if event.meta_event_type == 'lifecycle':
+    if event.meta_event_type == 'lifecycle' and not config.is_start:
+        config.is_start=True
         return True
     return False
 
 
-morning_metaevent = on_metaevent(rule=check_first_connect, block=True)
+start_metaevent = on_metaevent(rule=check_first_connect, block=True)
 
 
-@morning_metaevent.handle()
+@start_metaevent.handle()
 async def _(bot: Bot, event: Event, state: dict):
-    """ 启动时发送问好信息 """
+    """ 启动时发送启动成功信息 """
     await start()
