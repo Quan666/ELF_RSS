80e9bae71e2655eba7e25ed6c252c7967994e66d
NekoAria <990879119@qq.com>
2021-07-22 08:15:54 +0800

    ✨ 添加新特性 - 为 `show_all` 增加关键词过滤功能，同时优化 `show` 与 `show_all` 返回的订阅信息格式

--- src/plugins/ELF_RSS2/show_all.py
+++ src/plugins/ELF_RSS2/show_all.py
@@ -1,9 +1,12 @@
+import re
+
 from nonebot import on_command
 from nonebot import permission as su
-from nonebot.adapters.cqhttp import Bot, Event, GroupMessageEvent, permission
+from nonebot.adapters.cqhttp import Bot, Event, GroupMessageEvent, permission, unescape
 from nonebot.rule import to_me
 
 from .RSS import rss_class
+from .show_dy import handle_rss_list
 
 RSS_SHOW_ALL = on_command(
     "showall",
@@ -16,6 +19,12 @@ RSS_SHOW_ALL = on_command(
 
 @RSS_SHOW_ALL.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: dict):
+    args = str(event.get_message()).strip()  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
+    if args:
+        rss_name_search = unescape(args)  # 如果用户发送了参数则直接赋值
+    else:
+        rss_name_search = None
+
     group_id = None
     if isinstance(event, GroupMessageEvent):
         group_id = event.group_id
@@ -24,23 +33,18 @@ async def handle_first_receive(bot: Bot, event: Event, state: dict):
     if group_id:
         rss_list = rss.find_group(group=str(group_id))
         if not rss_list:
-            await RSS_SHOW_ALL.send("当前群组没有任何订阅！")
+            await RSS_SHOW_ALL.send("❌ 当前群组没有任何订阅！")
             return
     else:
         rss_list = rss.read_rss()
-    if rss_list:
-        if len(rss_list) == 1:
-            await RSS_SHOW_ALL.send(str(rss_list[0]))
-        else:
-            flag = 0
-            info = ""
-            for rss_tmp in rss_list:
-                if flag % 5 == 0 and flag != 0:
-                    await RSS_SHOW_ALL.send(str(info[:-2]))
-                    info = ""
-                info += f"Name：{rss_tmp.name}\nURL：{rss_tmp.url}\n\n"
-                flag += 1
-            await RSS_SHOW_ALL.send(f"{info}共 {flag} 条订阅")
 
+    if rss_name_search:
+        rss_list = [
+            i for i in rss_list if re.search(rss_name_search, f"{i.name}|{i.url}")
+        ]
+
+    if rss_list:
+        msg_str = await handle_rss_list(rss_list)
+        await RSS_SHOW_ALL.send(msg_str)
     else:
-        await RSS_SHOW_ALL.send("当前没有任何订阅！")
+        await RSS_SHOW_ALL.send("❌ 当前没有任何订阅！")
