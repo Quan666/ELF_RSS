8442c92c3da410bc05a9d95e587822aa5decc052
Quan666 <i@oy.mk>
2021-08-17 10:08:11 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ELF_RSS",
+    version="2.3.0",
+    author="Quan666",
+    author_email="i@oy.mk",
+    description="QQ机器人 RSS订阅 插件，订阅源建议选择 RSSHub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Quan666/ELF_RSS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
