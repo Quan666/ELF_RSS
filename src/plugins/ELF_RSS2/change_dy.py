196f76ce91cf7b0a8730d78caa1c25d03e7b9145
NekoAria <990879119@qq.com>
2021-09-14 17:05:11 +0800

    :bug: 修复 BUG - 修正 `修改订阅属性 name` 的逻辑

--- src/plugins/ELF_RSS2/change_dy.py
+++ src/plugins/ELF_RSS2/change_dy.py
@@ -11,7 +11,7 @@ from tinydb import TinyDB, Query
 
 from .RSS import my_trigger as tr
 from .RSS import rss_class
-from .config import JSON_PATH
+from .config import DATA_PATH, JSON_PATH
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 
@@ -98,11 +98,12 @@ attribute_dict = {
 
 
 # 处理要修改的订阅参数
-def handle_change_list(
+async def handle_change_list(
     rss: rss_class.Rss, key_to_change: str, value_to_change: str, group_id: int
 ):
     if key_to_change == "name":
-        rss.rename_file(value_to_change + ".json")
+        await tr.delete_job(rss)
+        rss.rename_file(DATA_PATH / (value_to_change + ".json"))
     # 暂时禁止群管理员修改 QQ / 群号，如要取消订阅可以使用 deldy 命令
     elif (key_to_change in ["qq", "qun"] and not group_id) or key_to_change == "mode":
         value_to_change = handle_property(
@@ -174,7 +175,7 @@ async def handle_rss_change(bot: Bot, event: Event, state: dict):
                 ):
                     await RSS_CHANGE.send(f"❌ 去重模式参数错误！\n{change_dict}")
                     return
-                handle_change_list(rss, key_to_change, value_to_change, group_id)
+                await handle_change_list(rss, key_to_change, value_to_change, group_id)
             else:
                 await RSS_CHANGE.send(f"❌ 参数错误或无权修改！\n{change_dict}")
                 return
