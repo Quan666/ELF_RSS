71f49f25d32e1eb536ccdb6098905fc8b0841dd7
NekoAria <990879119@qq.com>
2021-12-28 23:53:35 +0800

    :bug: 修复 BUG - pyquery 对开头是 url 的字符串默认当作 url 参数处理

--- src/plugins/ELF_RSS2/RSS/routes/Parsing/utils.py
+++ src/plugins/ELF_RSS2/RSS/routes/Parsing/utils.py
@@ -1,3 +1,5 @@
+import re
+
 import httpx
 
 from ....config import config
@@ -20,4 +22,7 @@ def get_proxy(open_proxy: bool) -> dict:
 
 # 获取正文
 def get_summary(item: dict) -> str:
-    return item["content"][0].get("value") if item.get("content") else item["summary"]
+    summary = item["content"][0].get("value") if item.get("content") else item["summary"]
+    if re.search("^https?://", summary):
+        return f"<div>{summary}</div>"
+    return summary
