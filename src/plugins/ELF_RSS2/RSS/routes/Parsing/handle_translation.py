f3ddc1d30a8d300973562ff4edede15344223cc8
Quan <litty01@qq.com>
2022-02-26 11:51:53 +0800

    :adhesive_bandage:  #223 变更 使用的Google 翻译库

--- src/plugins/ELF_RSS2/RSS/routes/Parsing/handle_translation.py
+++ src/plugins/ELF_RSS2/RSS/routes/Parsing/handle_translation.py
@@ -2,7 +2,8 @@ import re
 import unicodedata
 
 import emoji
-from google_trans_new import google_translator
+from googletrans import Translator
+from httpcore import SyncHTTPProxy
 
 from ....config import config
 from ....RSS import translation_baidu
@@ -10,7 +11,17 @@ from ....RSS import translation_baidu
 
 # 翻译
 async def handle_translation(content: str) -> str:
-    translator = google_translator()
+    if config.rss_proxy and len(config.rss_proxy.split(":")) == 2:
+        proxy = SyncHTTPProxy(
+            (
+                b"http",
+                bytes(config.rss_proxy.split(":")[0], encoding="utf-8"),
+                int(config.rss_proxy.split(":")[1]),
+                b"",
+            )
+        )
+        translator = Translator(proxies={"http": proxy, "https": proxy})
+    translator = Translator()
     try:
         text = emoji.demojize(content)
         text = re.sub(r":[A-Za-z_]*:", " ", text)
@@ -23,7 +34,7 @@ async def handle_translation(content: str) -> str:
                 translation_baidu.baidu_translate(re.escape(text))
             )
         else:
-            text = "\n翻译：\n" + str(translator.translate(re.escape(text), lang_tgt="zh"))
+            text = "\n翻译：\n" + str(translator.translate(re.escape(text), dest="zh-cn"))
         text = re.sub(r"\\", "", text)
         text = re.sub(r"百度翻译", "\n", text)
     except Exception as e:
