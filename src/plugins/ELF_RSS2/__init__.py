9276188b3e9ae8f3157787e04979e550a0402fe0
Kuro Latency <oozorakuro409@gmail.com>
2022-07-29 22:17:45 +0800

    只需监控一次元事件，避免bot重复响应 (#342)
    refactor: 重构启动元事件的处理逻辑

--- src/plugins/ELF_RSS2/__init__.py
+++ src/plugins/ELF_RSS2/__init__.py
@@ -1,8 +1,7 @@
 import asyncio
 
-import arrow
 from nonebot import on_metaevent, require
-from nonebot.adapters.onebot.v11 import Bot, Event, LifecycleMetaEvent
+from nonebot.adapters.onebot.v11 import Bot
 from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
 
@@ -21,16 +20,7 @@ __plugin_meta__ = PluginMetadata(
 )
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
-START_TIME = arrow.now()
-
-
-async def check_first_connect(event: Event) -> bool:
-    return isinstance(event, LifecycleMetaEvent) and arrow.now() < START_TIME.shift(
-        minutes=1
-    )
-
-
-start_metaevent = on_metaevent(rule=check_first_connect, block=True)
+start_metaevent = on_metaevent(rule=lambda: True, temp=True)
 
 
 # 启动时发送启动成功信息
