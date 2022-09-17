fbf051ae4470b9b7d9662c2d1e1c0c6691137910
Nisekoi5 <49276902+Nisekoi5@users.noreply.github.com>
2022-09-17 15:59:11 +0800

    Create __init__.py

---
+++ src/plugins/ELF_RSS2/__init__.py
@@ -0,0 +1,51 @@
+import asyncio
+
+from nonebot import on_metaevent, require
+from nonebot.adapters.onebot.v11 import Bot, LifecycleMetaEvent
+from nonebot.log import logger
+from nonebot.plugin import PluginMetadata
+
+from . import command
+from . import my_trigger as tr
+from .config import DATA_PATH
+from .rss_class import Rss
+from .utils import send_message_to_admin
+
+VERSION = "2.6.12"
+__plugin_meta__ = PluginMetadata(
+    name="ELF_RSS",
+    description="QQ机器人 RSS订阅 插件，订阅源建议选择 RSSHub",
+    usage="https://github.com/Quan666/ELF_RSS",
+    extra={"author": "Quan666 <i@Rori.eMail>", "version": VERSION},
+)
+
+scheduler = require("nonebot_plugin_apscheduler").scheduler
+
+
+async def check_first_connect(_: LifecycleMetaEvent) -> bool:
+    return True
+
+
+start_metaevent = on_metaevent(rule=check_first_connect, temp=True)
+
+
+# 启动时发送启动成功信息
+@start_metaevent.handle()
+async def start(bot: Bot) -> None:
+
+    # 启动后检查 data 目录，不存在就创建
+    if not DATA_PATH.is_dir():
+        DATA_PATH.mkdir()
+
+    boot_message = (
+        f"Version: v{VERSION}\nAuthor：Quan666\nhttps://github.com/Quan666/ELF_RSS"
+    )
+
+    rss_list = Rss.read_rss()  # 读取list
+    if not rss_list:
+        await send_message_to_admin(f"第一次启动，你还没有订阅，记得添加哟！\n{boot_message}", bot)
+        logger.info("第一次启动，你还没有订阅，记得添加哟！")
+    await send_message_to_admin(f"ELF_RSS 订阅器启动成功！\n{boot_message}", bot)
+    logger.info("ELF_RSS 订阅器启动成功！")
+    # 创建检查更新任务
+    await asyncio.gather(*[tr.add_job(rss) for rss in rss_list if not rss.stop])
