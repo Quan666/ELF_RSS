f657e80a359a5b62cc321c0a95b62c3b9174cae4
Neko Aria <990879119@qq.com>
2022-10-04 01:01:20 +0800

    chore(handle_translation): 优化逻辑和样式

--- src/plugins/ELF_RSS2/parsing/handle_translation.py
+++ src/plugins/ELF_RSS2/parsing/handle_translation.py
@@ -1,19 +1,17 @@
 import hashlib
 import random
 import re
+from typing import Dict, Optional
 
 import aiohttp
 import emoji
-from deep_translator import GoogleTranslator, DeeplTranslator, single_detection
+from deep_translator import DeeplTranslator, GoogleTranslator, single_detection
 from nonebot.log import logger
 
 from ..config import config
 
 
-async def baidu_translator(content: str) -> str:
-    appid = config.baidu_id
-    secret_key = config.baidu_key
-
+async def baidu_translator(content: str, appid: str, secret_key: str) -> str:
     url = "https://api.fanyi.baidu.com/api/trans/vip/translate"
     salt = str(random.randint(32768, 65536))
     sign = hashlib.md5((appid + content + salt + secret_key).encode()).hexdigest()
@@ -30,15 +28,14 @@ async def baidu_translator(content: str) -> str:
         data = await resp.json()
         try:
             content = "".join(i["dst"] + "\n" for i in data["trans_result"])
-            text = "\n百度翻译：\n" + content[:-1]
-            return text
-        except Exception:
+            return "\n百度翻译：\n" + content[:-1]
+        except Exception as e:
             error_msg = f"百度翻译失败：{data['error_msg']}"
             logger.warning(error_msg)
-            raise Exception(error_msg)
+            raise Exception(error_msg) from e
 
 
-async def google_translation(text: str, proxies: dict) -> str:
+async def google_translation(text: str, proxies: Optional[Dict[str, str]]) -> str:
     # text 是处理过emoji的
     try:
         translator = GoogleTranslator(source="auto", target="zh-CN", proxies=proxies)
@@ -46,25 +43,26 @@ async def google_translation(text: str, proxies: dict) -> str:
     except Exception as e:
         error_msg = "\nGoogle翻译失败：" + str(e) + "\n"
         logger.warning(error_msg)
-        raise Exception(error_msg)
+        raise Exception(error_msg) from e
 
 
-async def deepl_translator(text: str, proxies: dict) -> str:
+async def deepl_translator(text: str, proxies: Optional[Dict[str, str]]) -> str:
     try:
         lang = None
         if config.single_detection_api_key:
             lang = single_detection(text, api_key=config.single_detection_api_key)
-        return "\nDeepl翻译：\n" + DeeplTranslator(
+        translator = DeeplTranslator(
             api_key=config.deepl_translator_api_key,
             source=lang,
             target="zh",
             use_free_api=True,
             proxies=proxies,
-        ).translate(re.escape(text))
+        )
+        return "\nDeepl翻译：\n" + str(translator.translate(re.escape(text)))
     except Exception as e:
         error_msg = "\nDeeplTranslator翻译失败：" + str(e) + "\n"
         logger.warning(error_msg)
-        raise Exception(error_msg)
+        raise Exception(error_msg) from e
 
 
 # 翻译
@@ -88,10 +86,12 @@ async def handle_translation(content: str) -> str:
             if config.deepl_translator_api_key:
                 text = await deepl_translator(text=text, proxies=proxies)
             elif config.baidu_id and config.baidu_key:
-                text = await baidu_translator(content=content)
+                text = await baidu_translator(
+                    content, config.baidu_id, config.baidu_key
+                )
             else:
                 google_translator_flag = True
-        except:
+        except Exception:
             google_translator_flag = True
         if google_translator_flag:
             text = await google_translation(text=text, proxies=proxies)
