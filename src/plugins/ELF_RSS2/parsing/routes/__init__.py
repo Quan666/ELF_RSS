28782619bef486e7cb29a44808e6a2761ea02603
Neko Aria <990879119@qq.com>
2023-03-22 18:39:49 +0800

    refactor(routes): 使用自动扫描的方式自动导入路由规则

--- src/plugins/ELF_RSS2/parsing/routes/__init__.py
+++ src/plugins/ELF_RSS2/parsing/routes/__init__.py
@@ -1,11 +1,12 @@
-from . import (
-    bilibili,
-    danbooru,
-    nga,
-    pixiv,
-    south_plus,
-    twitter,
-    weibo,
-    yande_re,
-    youtube,
-)
+from typing import List
+
+
+def __list_all_modules() -> List[str]:
+    from pathlib import Path
+
+    module_paths = list(Path(__file__).parent.glob("*.py"))
+    return [module.name[:-3] for module in module_paths if module.name != "__init__.py"]
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
