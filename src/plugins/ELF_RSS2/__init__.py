be1d57b36f2ecaffcd5bbc765170ddedaaf4f3a3
Neko Aria <990879119@qq.com>
2023-03-27 09:05:22 +0800

    feat(config): 新增配置项 `enable_boot_message` ，用来控制启动后是否发送提示消息 (close #412)

--- src/plugins/ELF_RSS2/__init__.py
+++ src/plugins/ELF_RSS2/__init__.py
@@ -7,7 +7,7 @@ from nonebot.plugin import PluginMetadata
 
 from . import command
 from . import my_trigger as tr
-from .config import DATA_PATH
+from .config import DATA_PATH, config
 from .rss_class import Rss
 from .utils import send_message_to_admin
 
@@ -44,9 +44,11 @@ async def start(bot: Bot) -> None:
 
     rss_list = Rss.read_rss()  # 读取list
     if not rss_list:
-        await send_message_to_admin(f"第一次启动，你还没有订阅，记得添加哟！\n{boot_message}", bot)
+        if config.enable_boot_message:
+            await send_message_to_admin(f"第一次启动，你还没有订阅，记得添加哟！\n{boot_message}", bot)
         logger.info("第一次启动，你还没有订阅，记得添加哟！")
-    await send_message_to_admin(f"ELF_RSS 订阅器启动成功！\n{boot_message}", bot)
+    if config.enable_boot_message:
+        await send_message_to_admin(f"ELF_RSS 订阅器启动成功！\n{boot_message}", bot)
     logger.info("ELF_RSS 订阅器启动成功！")
     # 创建检查更新任务
     await asyncio.gather(*[tr.add_job(rss) for rss in rss_list if not rss.stop])
--- src/plugins/ELF_RSS2/config.py
+++ src/plugins/ELF_RSS2/config.py
@@ -17,9 +17,10 @@ class ELFConfig(BaseConfig):
     rss_proxy: Optional[str] = None
     rsshub: AnyHttpUrl = "https://rsshub.app"  # type: ignore
     rsshub_backup: List[AnyHttpUrl] = []
-    db_cache_expire = 30
-    limit = 200
+    db_cache_expire: int = 30
+    limit: int = 200
     max_length: int = 1024  # 正文长度限制，防止消息太长刷屏，以及消息过长发送失败的情况
+    enable_boot_message: bool = True
 
     zip_size: int = 2 * 1024
     gif_zip_size: int = 6 * 1024
@@ -32,22 +33,20 @@ class ELFConfig(BaseConfig):
     baidu_id: Optional[str] = None
     baidu_key: Optional[str] = None
     deepl_translator_api_key: Optional[str] = None
-    single_detection_api_key: Optional[
-        str
-    ] = None  # 配合 deepl_translator 使用的语言检测接口，前往https://detectlanguage.com/documentation注册获取api_key
+    # 配合 deepl_translator 使用的语言检测接口，前往 https://detectlanguage.com/documentation 注册获取 api_key
+    single_detection_api_key: Optional[str] = None
 
     qb_username: Optional[str] = None  # qbittorrent 用户名
     qb_password: Optional[str] = None  # qbittorrent 密码
     qb_web_url: Optional[str] = None
-    qb_down_path: Optional[str] = None  # qb的文件下载地址，这个地址必须是 go-cqhttp能访问到的
+    qb_down_path: Optional[str] = None  # qb 的文件下载地址，这个地址必须是 go-cqhttp 能访问到的
     down_status_msg_group: Optional[List[int]] = None
     down_status_msg_date: int = 10
 
     pikpak_username: Optional[str] = None  # pikpak 用户名
     pikpak_password: Optional[str] = None  # pikpak 密码
-    pikpak_download_path: str = (
-        ""  # pikpak 离线保存的目录, 默认是根目录，示例: ELF_RSS/Downloads ,目录不存在会自动创建, 不能/结尾
-    )
+    # pikpak 离线保存的目录, 默认是根目录，示例: ELF_RSS/Downloads ,目录不存在会自动创建, 不能/结尾
+    pikpak_download_path: str = ""
 
 
 config = ELFConfig(**get_driver().config.dict())
